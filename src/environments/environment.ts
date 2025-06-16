1e25fefb3545d54566b6938a7d51ef4bf3fb80c9
Michael Sowah <sowahm808@gmail.com>
2025-06-15 23:37:48 -0500

    Initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,14 +2,19 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// environment.ts
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'YOUR_API_KEY',
-    authDomain: 'YOUR_AUTH_DOMAIN',
-    projectId: 'YOUR_PROJECT_ID',
-  },
+    apiKey: "AIzaSyBHwdDdJmAQevqL83_JkQtmEynzAsihU04",
+    authDomain: "your-project-id.firebaseapp.com",
+    projectId: "kidsapp-92fb2",
+    storageBucket: "your-project-id.appspot.com",
+    messagingSenderId: "1234567890",
+    appId: "1:1234567890:web:abc123xyz"
+  }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
