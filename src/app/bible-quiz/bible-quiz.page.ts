1433cd952710265212a392fd5c6ca81c16080c5d
Michael Sowah <sowahm808@gmail.com>
2025-06-28 22:09:29 -0500

    updated

--- src/app/bible-quiz/bible-quiz.page.ts
+++ src/app/bible-quiz/bible-quiz.page.ts
@@ -13,6 +13,7 @@ import {
   IonButton,
   IonRadio,
   IonRadioGroup,
+  IonSpinner,
 } from '@ionic/angular/standalone';
 import { BibleQuizApiService } from '../services/bible-quiz-api.service';
 import { BibleQuestion } from '../models/bible-quiz';
@@ -34,6 +35,7 @@ import { BibleQuestion } from '../models/bible-quiz';
     IonButton,
     IonRadio,
     IonRadioGroup,
+    IonSpinner
   ],
   templateUrl: './bible-quiz.page.html',
   styleUrls: ['./bible-quiz.page.scss'],
