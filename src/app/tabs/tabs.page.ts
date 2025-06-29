0a496d0bf9eee368400bac5178a64b48624ba709
Michael Sowah <sowahm808@gmail.com>
2025-06-29 00:33:46 -0500

    updated

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
 import {
   IonTabs,
- 
   IonTabBar,
   IonTabButton,
   IonButton,
   IonIcon,
   IonLabel,
+  IonButtons,
+  IonTitle,
+  IonToolbar,
+  IonHeader,
 } from '@ionic/angular/standalone';
 
 import { RoleService } from '../services/role.service';
 import { FirebaseService } from '../services/firebase.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
@@ -25,12 +29,31 @@ import { FirebaseService } from '../services/firebase.service';
     IonButton,
     IonIcon,
     IonLabel,
+    NgIf,
+    IonButtons,
+    IonTitle,
+    IonToolbar,
+    IonHeader
   ],
   templateUrl: './tabs.page.html',
   styleUrls: ['./tabs.page.scss'],
 })
 export class TabsPage {
-  constructor(public roleSvc: RoleService, private fb: FirebaseService) {}
+    loggedIn = false;
+
+  constructor(private router: Router,public roleSvc: RoleService, private fb: FirebaseService) {
+       this.fb.auth.onAuthStateChanged((user) => {
+      this.loggedIn = !!user;
+      const url = this.router.url;
+      if (!user) {
+        if (!url.startsWith('/login') && !url.startsWith('/register')) {
+          this.router.navigateByUrl('/login');
+        }
+      } else if (url === '/login' || url === '/register' || url === '/') {
+        this.router.navigateByUrl('/tabs');
+      }
+    });
+  }
 
   logout() {
     this.roleSvc.setRole(null);
