ae7ecffe696b1cff62fe05497f13f4222bff4870
sowahm808 <sowahm808@gmail.com>
2025-06-28 20:31:31 -0500

    fix(clickable): enable ionic events

--- src/zone-flags.ts
+++ src/zone-flags.ts
@@ -3,4 +3,6 @@
  * running with certain Web Component callbacks
  */
 // eslint-disable-next-line no-underscore-dangle
-(window as any).__Zone_disable_customElements = true;
+// Disabling this flag prevents Ionic components from emitting events that
+// Angular can listen to. Commented out so form elements remain interactive.
+// (window as any).__Zone_disable_customElements = true;
