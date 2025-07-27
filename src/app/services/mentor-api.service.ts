b3a0ba77cdf8f34ae130c1fb7e1457e0fb073f7d
Michael Sowah <sowahm808@gmail.com>
2025-07-26 20:39:19 -0500

    updated

--- src/app/services/mentor-api.service.ts
+++ src/app/services/mentor-api.service.ts
@@ -9,7 +9,7 @@ import { MentorAssignment, MentorChildren } from '../models/mentor-assignment';
 @Injectable({ providedIn: 'root' })
 export class MentorApiService {
   private apiEnabled = !!environment.apiUrl;
-  private readonly baseUrl = `${environment.apiUrl}/api/mentors`;
+  private readonly baseUrl = `${environment.apiUrl}/api/mentor`;
 
   constructor(private http: HttpClient, private fb: FirebaseService) {}
 
