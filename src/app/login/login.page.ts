24f1f497e02142a8cf887457bc935a2d7215cb4b
Michael Sowah <sowahm808@gmail.com>
2025-08-03 22:53:26 -0500

    updated

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonItem, IonLabel, IonButton, IonList, IonSelect, IonSelectOption } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonItem, IonLabel, IonButton, IonList, IonSelect, IonSelectOption,IonImg } from '@ionic/angular/standalone';
 import { RouterLink } from '@angular/router';
 import { FirebaseService } from '../services/firebase.service';
 import { Router } from '@angular/router';
@@ -27,6 +27,7 @@ import { ToastController } from '@ionic/angular';
     IonSelect,
     IonSelectOption,
     RouterLink,
+    IonImg,
   ],
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
